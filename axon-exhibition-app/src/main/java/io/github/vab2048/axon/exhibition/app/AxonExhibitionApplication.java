3309a30bc462197a5591239b96ef74a3a5f494c8
vab2048 <vab2048@gmail.com>
2022-05-04 14:22:39 +0100

    Create initial AxonExhibitionApplication

---
+++ axon-exhibition-app/src/main/java/io/github/vab2048/axon/exhibition/app/AxonExhibitionApplication.java
@@ -0,0 +1,13 @@
+package io.github.vab2048.axon.exhibition.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonExhibitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonExhibitionApplication.class, args);
+    }
+
+}
---
+++ axon-exhibition-app/src/test/java/vab2048/axon/postgres/demo/AxonExhibitionApplicationTests.java
@@ -0,0 +1,13 @@
+package vab2048.axon.postgres.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonExhibitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
