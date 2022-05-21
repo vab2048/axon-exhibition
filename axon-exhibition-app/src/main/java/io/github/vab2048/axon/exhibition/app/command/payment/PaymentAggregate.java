418c417077777f40f351fd6c70066c9fc59f73d6
vab2048 <vab2048@gmail.com>
2022-05-21 23:15:31 +0100

    Update PaymentAggregate

--- axon-exhibition-app/src/main/java/io/github/vab2048/axon/exhibition/app/command/payment/PaymentAggregate.java
+++ axon-exhibition-app/src/main/java/io/github/vab2048/axon/exhibition/app/command/payment/PaymentAggregate.java
@@ -6,6 +6,8 @@ import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
@@ -16,6 +18,7 @@ import static org.axonframework.modelling.command.AggregateLifecycle.apply;
  */
 @Aggregate
 public class PaymentAggregate {
+    private static final Logger log = LoggerFactory.getLogger(PaymentAggregate.class);
 
     @AggregateIdentifier
     private UUID paymentId;
@@ -41,11 +44,33 @@ public class PaymentAggregate {
 
     @CommandHandler
     public void handle(MarkPaymentAsCompletedCommand command) {
+        // If the payment status is already COMPLETED then we log a warning (because something could be wrong and worth inspecting)
+        // and just return (for idempotency).
+        if(status.equals(PaymentStatus.COMPLETED)) {
+            log.warn("Received MarkPaymentAsCompletedCommand ({}) for payment which is currently already in the COMPLETED status. Will ignore the command.", command);
+            return;
+        }
+        // If the status is FAILED, then we need to reject this command.
+        if(status.equals(PaymentStatus.FAILED)) {
+            throw new IllegalStateException("Unable to transition to COMPLETED state because the payment has already FAILED.");
+        }
+        // Otherwise, we can go ahead and apply the event...
         apply(new PaymentCompletedEvent(command.paymentId()));
     }
 
     @CommandHandler
     public void handle(MarkPaymentAsFailedCommand command) {
+        // If the payment status is already FAILED then we log a warning (because something could be wrong and worth inspecting)
+        // and just return (for idempotency).
+        if(status.equals(PaymentStatus.FAILED)) {
+            log.warn("Received MarkPaymentAsCompletedCommand ({}) for payment which is currently already in the FAILED status. Will ignore the command.", command);
+            return;
+        }
+        // If the status is COMPLETED, then we need to reject this command.
+        if(status.equals(PaymentStatus.COMPLETED)) {
+            throw new IllegalStateException("Unable to transition to FAILED state because the payment has already COMPLETED.");
+        }
+        // Otherwise, we can go ahead and apply the event...
         apply(new PaymentFailedEvent(command.paymentId()));
     }
 
